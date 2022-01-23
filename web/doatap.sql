a9d71f77bbf9fab971d8abd5ef31c079f42036b0
SpyrosNavro <59921293+SpyrosNavro@users.noreply.github.com>
2022-01-23 15:34:49 +0200

    added applications

--- web/doatap.sql
+++ web/doatap.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 22, 2022 at 11:19 AM
+-- Generation Time: Jan 23, 2022 at 02:31 PM
 -- Server version: 10.4.22-MariaDB
--- PHP Version: 7.4.27
+-- PHP Version: 8.1.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,26 @@ SET time_zone = "+00:00";
 --
 -- Database: `doatap`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `applications`
+--
+
+CREATE TABLE `applications` (
+  `id_of_application` int(11) NOT NULL,
+  `country_of_university` varchar(25) NOT NULL,
+  `university` varchar(50) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `ects` int(11) NOT NULL,
+  `registration_date` date NOT NULL,
+  `graduation_date` date NOT NULL,
+  `certificate` blob NOT NULL,
+  `id_card` blob NOT NULL,
+  `research_or_work` blob NOT NULL,
+  `id_of_person` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -47,11 +67,18 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `firstname`, `lastname`, `country`, `addr`, `city`, `region`, `email`, `tel`, `postcode`, `psw`, `user_type`) VALUES
-(6, 'Kostas', 'Papadopoulos', 'Greece', 'Marousi 12', 'Athens', 'Marousi', 'kostas@gmail.com', '1234567891', '12345', '123', 'Manager');
+(6, 'Kostas', 'Papadopoulos', 'Greece', 'Marousi 12', 'Athens', 'Marousi', 'kostas@gmail.com', '1234567891', '12345', '123', 'Manager'),
+(11, 'spyros', 'navro', 'greece', 'somewhere', 'athens', 'marousi', 'spyros@gmail.com', '6969696969', '12345', '123', 'Common');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `applications`
+--
+ALTER TABLE `applications`
+  ADD PRIMARY KEY (`id_of_application`);
 
 --
 -- Indexes for table `user`
@@ -64,10 +91,16 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `applications`
+--
+ALTER TABLE `applications`
+  MODIFY `id_of_application` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
